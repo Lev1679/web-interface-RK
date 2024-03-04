a79fdba4c92b201d3b194a3cedafceacf2ac5aa2
Lev1679 <leozverev@bk.ru>
2024-03-04 15:31:29 +0300

    Принтер бургеров

--- RK-interface-HTTP/javascript/printerlist.js
+++ RK-interface-HTTP/javascript/printerlist.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Здесь мы используем XMLHttpRequest для получения содержимого страницы
-    const url = 'http://192.168.5.7/#/';
+    const apiKey = 'c326c12042de41bfa7ca92905d2b32ff';
+    const apiUrl = 'http://127.0.0.1:7125/printer/objects/query?extruder'; // обновите этот URL
+
     const xhr = new XMLHttpRequest();
-  
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Парсим HTML-страницу и извлекаем данные о температуре
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(xhr.responseText, 'text/html');
-  
-        const tempActualElement = doc.getElementById('tempActual'); // Замените 'tempActual' на реальный идентификатор элемента
-        const heaterBedElement = doc.getElementById('heaterBed'); // Замените 'heaterBed' на реальный идентификатор элемента
-  
-        // Получаем значения температур и обновляем элементы на странице
-        const tempActual = tempActualElement ? tempActualElement.innerText : 'Не найдено';
-        const heaterBed = heaterBedElement ? heaterBedElement.innerText : 'Не найдено';
-  
-        document.getElementById('tempActualDisplay').innerText = `Температура: ${tempActual}`;
-        document.getElementById('heaterBedDisplay').innerText = `Температура heated bed: ${heaterBed}`;
-      }
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                const extruderTemperature = response.temperature || 'Не найдено';
+
+                document.getElementById('extruderTemperatureDisplay').innerText = `Температура экструдера: ${extruderTemperature}`;
+            } else {
+                console.error(`Ошибка при запросе: ${xhr.status}`);
+            }
+        }
     };
-  
-    xhr.open('GET', url, true);
+
+    xhr.open('GET', apiUrl, true);
+    xhr.setRequestHeader('Authorization', `Bearer ${apiKey}`);
     xhr.send();
-  });
+});
+
+// Мы пытались через Javascript, но ничего не получилось
