2252c6cf9d99b2e2e062c827625cc1a7d5ab05ca
Lev1679 <leozverev@bk.ru>
2024-03-14 14:40:06 +0300

    Окощко для жопы

--- RK-interface-HTTP/javascript/modalwindows.js
+++ RK-interface-HTTP/javascript/modalwindows.js
@@ -1,39 +1,39 @@
 function modalController({modal, btnOpen, btnClose, time = 300}) {
-  const modalElem = document.querySelector(modal);
-  
-  modalElem.style.transition = `opacity ${time}ms ease-in-out`;
-  modalElem.style.opacity = 0;
-  modalElem.style.visibility = 'hidden';
-  
-  const closeModal = event => {
-      const target = event.target;
-  
-      if (
-          target === modalElem ||
-          (btnClose && target.closest(btnClose)) ||
-          event.code === 'Escape'
-      ) {
-          modalElem.style.opacity = 0;
-          setTimeout(() => {
-              modalElem.style.visibility = 'hidden';
-          }, time);
-          window.removeEventListener('keydown', closeModal);
-      }
-  }
-  
-  const openModal = () => {
-      modalElem.style.visibility = 'visible';
-      modalElem.style.opacity = 1;
-      window.addEventListener('keydown', closeModal);
-  };
-  
-  document.querySelector(btnOpen).addEventListener('click', openModal);
-  
-  modalElem.addEventListener('click', closeModal);
+    const modalElem = document.querySelector(modal);
+    
+    modalElem.style.transition = `opacity ${time}ms ease-in-out`;
+    modalElem.style.opacity = 0;
+    modalElem.style.visibility = 'hidden';
+    
+    const closeModal = event => {
+        const target = event.target;
+    
+        if (
+            target === modalElem ||
+            (btnClose && target.closest(btnClose)) ||
+            event.code === 'Escape'
+        ) {
+            modalElem.style.opacity = 0;
+            setTimeout(() => {
+                modalElem.style.visibility = 'hidden';
+            }, time);
+            window.removeEventListener('keydown', closeModal);
+        }
+    }
+    
+    const openModal = () => {
+        modalElem.style.visibility = 'visible';
+        modalElem.style.opacity = 1;
+        window.addEventListener('keydown', closeModal);
+    };
+    
+    document.querySelector(btnOpen).addEventListener('click', openModal);
+    
+    modalElem.addEventListener('click', closeModal);
 }
 
 modalController({
-  modal: 'modal',
-  btnOpen: 'request_button',
-  btnClose: 'modal_close',
+    modal: '.modal',
+    btnOpen: '.request_button',
+    btnClose: '.modal_close',
 });
